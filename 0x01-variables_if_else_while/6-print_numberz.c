3be6f0b2e0ce38c76e25d851acc184dff5a63936
fortuna-es <fortunaes98@gmail.com>
2022-03-11 09:19:24 -0800

    updated

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * You are not allowed to use any variables of type char.
+ * You can only use `putchar` to print to consol.
+ * You can onlu use `puchar` twice.
+ * Return: 0
+ */
+int main(void)
+{
+	int i'
+		for (i = 0; i < 10; i++)
+		{
+			putchar(i + '0');
+		}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-_print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print he alphabet in reverse order in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	char 0;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
