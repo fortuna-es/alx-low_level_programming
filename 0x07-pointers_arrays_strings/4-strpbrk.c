6961faf0fda376a9367378d8bf531add4423f277
fortuna-es <56885624+fortuna-es@users.noreply.github.com>
2022-03-28 22:53:25 +0300

    updated

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,24 +1,30 @@
 #include "main.h"
 
 /**
- * _strpbrk - Search a string for any of a set of bytes.
- * @s: string
- * @accept: string to match
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
+ * _strpbrk - locates the first occurrence in the string s of any
+ * of the bytes in the string accept
+ * @s: for the char
+ * @accept: paramter for accept
+ * Return: pointer to the byte s
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
-	char *p;
+	int i, t, j;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		j = 0;
-		while (accept[j] != '\0')
+		t = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[j] == s[i])
-			{
-
+			if (s[i] == accept[j])
+				t = 1;
+		}
+		j = 0;
+		if (t == 1)
+			return (s + i);
+		i++;
+	}
+	return (0);
+}
