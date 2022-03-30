093eb179909bdd9a4ee33828605c5dc95838436b
fortuna-es <56885624+fortuna-es@users.noreply.github.com>
2022-03-30 10:59:27 +0300

    0x08-recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two strings with "wildcard expansion" capabilities
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if strings can be considered identical, else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 == '\0' || *s2 == '\0')
+	{
+		if (*s1 == '\0' && *s2 == '*')
+			return wildcmp(s1, ++s2);
+		else if (*s1 == '*' && *s2 == '\0')
+			return wildcmp(++s1, s2);
+		return (0);
+	}
+
+	if (*s1 == *s2)
+	{
+		return wildcmp(++s1, ++s2);
+	}
+
