24b6eb0b453cd13dc419cbc7585a7bccd31918b2
fortuna-es <56885624+fortuna-es@users.noreply.github.com>
2022-04-07 19:01:26 +0300

    0x0c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,17 @@
-#include <stdlib.h>
+#include "main.h"
+
 /**
-  * malloc_checked - allocates memory using malloc.
-  * @b: size of memory to be allocated.
-  *
-  * Return: pointer to allocated memory.
-  */
+ * malloc_checked - allocate memory using malloc.
+ * Description: If malloc fails, terminate process with status 98.
+ * @b: unsigned int memory size to allocate
+ * Return: void pointer to malloc'd memory space
+ */
+
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
+	void *p;
 
+	p = malloc(b);
 	if (p == NULL)
 		exit(98);
 
