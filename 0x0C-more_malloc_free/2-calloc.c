b6c3a246aa0b06f7585cd171b3e81a47cf9b9f20
fortuna-es <56885624+fortuna-es@users.noreply.github.com>
2022-04-07 19:36:58 +0300

    0x0c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,42 +1,31 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-
 /**
- *_calloc - allocate memory for an array usingmalloc
- * @nmemb: number ofelements
- * @size: size of bytes
- * Return: pointer
- */
-
+  * _calloc - allocates memory of an array using malloc.
+  * @nmemb: number of elements in array.
+  * @size: size of elements of array.
+  *
+  * Return: NULL is size or nmemb == 0.
+  * NULL if malloc fails.
+  * Pointer to memory allocated if successful.
+  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  void *ptr;
+	void *p;
+	unsigned int i;
 
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  ptr = malloc(nmemb * size);
-  if (ptr == NULL)
-    return (NULL);
-  _memset(ptr, 0, (nmemb * size));
-  return (ptr);
-}
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 
-/**
- *_memset - sets first n bytes of the memory area
- * @s: array to set
- * @b: what to set it to
- * @n: first n bytes
- * Return: s
- */
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
 
-char *_memset(char *s, char b, unsigned int n)
-{
-  unsigned int i;
-
-  for (i = 0; i < n; i++)
-    {
-      s[i] = b;
-    }
-  return (s);
+	return (p);
 }
