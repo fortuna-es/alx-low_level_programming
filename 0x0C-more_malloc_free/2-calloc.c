c13aa0cbf4c17810c68b074b88a7f659cadaf92c
fortuna-es <56885624+fortuna-es@users.noreply.github.com>
2022-04-09 03:46:26 +0300

    updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,27 +1,44 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
-  * _calloc - ...
-  * @nmemb: number of members
-  * @size: size
-  *
-  * Return: ...
-  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0, l = 0;
-	char *p;
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	l = nmemb * size;
-	p = malloc(l);
-	if (p == NULL)
-		return (NULL);
-	while (i < l)
-	{
-		p[i] = 0;
-		i++;
-	}
-	return (p);
+char *ptr = s;
+
+while (n--)
+	*s++ = b;
+
+return (ptr);
 }
 
+/**
+ * *_calloc - allocates memory for an array, using malloc
+ * @nmemb: array length
+ * @size: size of each element
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *m;
+
+if (size == 0 || nmemb == 0)
+	return (NULL);
+
+m = malloc(nmemb * size);
+
+if (m == 0)
+	return (NULL);
+
+_memset(m, 0, nmemb * size);
+
+return (m);
+}
